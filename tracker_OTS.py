a6e51d13d2c6f6e391b1fdcae426b7dafd9f4ba4
dhingratul <atul.dhingra@rutgers.edu>
2017-05-12 22:51:22 -0400

    pep-8

--- tracker_OTS.py
+++ tracker_OTS.py
@@ -9,9 +9,8 @@ Track a player annotated with Bounded box using OpenCV example
 
 import cv2
 import sys
- 
-if __name__ == '__main__' :
-     
+if __name__ == '__main__':
+
     tracker = cv2.Tracker_create("KCF")
     video = cv2.VideoCapture("clip.mp4")
     if not video.isOpened():
@@ -21,9 +20,9 @@ if __name__ == '__main__' :
     if not frame_init:
         print('Error: Cannot read video file')
         sys.exit()
-     
+
     # Define an initial bounding box
-    #bbox = (287, 23, 86, 320)
+    # bbox = (287, 23, 86, 320)
 
     bbox = cv2.selectROI(frame, False)
     # Initialize tracker with first frame and bounding box
@@ -35,16 +34,17 @@ if __name__ == '__main__' :
             break
         # Update tracker
         frame_update, bbox = tracker.update(frame)
- 
+
         # Draw bounding box
         if frame_update:
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (0,0,255))
- 
+            cv2.rectangle(frame, p1, p2, (0, 0, 255))
+
         # Display result
         cv2.imshow("Tracking", frame)
- 
+
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
-        if k == 27 : break
+        if k == 27:
+                break
